bb9acf8a55c99668f67ce11d740592d891b43fd6
TanmayThakur2209 <ttanmay22@gmail.com>
2025-07-30 23:24:44 +0530

    Update db.js

--- Onlyfollowers/server/config/db.js
+++ Onlyfollowers/server/config/db.js
@@ -1,28 +1,11 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
-
-// const uri = process.env.MONGO_URI;
-
-
-const uri = "mongodb+srv://ttanmay22:Tanmay22@onlyfollowers.owwlobo.mongodb.net/?retryWrites=true&w=majority&appName=Onlyfollowers";
-
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    if (!client.topology?.isConnected()) {
-      await client.connect();
-      console.log("✅ Connected to MongoDB Atlas");
-    }
-    return client;
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log('✅ MongoDB connected');
   } catch (err) {
-    console.error("❌ MongoDB connection failed:", err);
+    console.error('❌ MongoDB connection failed:', err);
     throw err;
   }
 };
