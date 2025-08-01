01925b4841b1df41381b2900fd3ab55a63f70a1c
TanmayThakur2209 <ttanmay22@gmail.com>
2025-08-01 21:10:02 +0530

    Update db.js

--- Onlyfollowers/server/config/db.js
+++ Onlyfollowers/server/config/db.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      directConnection: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error);
