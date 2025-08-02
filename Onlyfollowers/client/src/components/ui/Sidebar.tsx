cba57ac9fcb56544e7f2112f1d51813d0447afa2
TanmayThakur2209 <ttanmay22@gmail.com>
2025-08-02 21:49:34 +0530

    Update Sidebar.tsx

--- Onlyfollowers/client/src/components/ui/Sidebar.tsx
+++ Onlyfollowers/client/src/components/ui/Sidebar.tsx
@@ -123,11 +123,19 @@ function Sidebar({}) {
       <div className="duration-200 transition-all">
 
       {isCollapsed ? (<div className="bottom-0 border-amber-50 border-2 m-2 rounded-full items-center flex">
-        <img src={`https://onlyfollowers.onrender.com/uploads/${user?.profilePhoto.path}`} className="p-1 rounded-full h-10 w-10"/> </div>):
+        {user?.profilePhoto?.path ? (
+          <img src={`http://localhost:5000/uploads/${user.profilePhoto.path}`} alt={user.username} className="m-1 rounded-full h-10 w-10"/>) 
+          : 
+          (<div className="m-1 rounded-full h-10 w-10 bg-[#d6480b] flex items-center justify-center text-white">
+            {user?.username?.[0] || "U"}</div>)} </div>):
 
        ( <div ref={moreRef} className="bottom-0  border-amber-50 border-2 m-2 duration-200 transition-all rounded-xl px-1 justify-between items-center flex">
         <div className="flex items-center">
-        <img src={user?.profilePhoto?.path ? `https://onlyfollowers.onrender.com/uploads/${user?.profilePhoto.path}`:``} className="m-1 rounded-full h-10 w-10"/>
+        {user?.profilePhoto?.path ? (
+          <img src={`http://localhost:5000/uploads/${user.profilePhoto.path}`} alt={user.username} className="m-1 rounded-full h-10 w-10"/>) 
+          : 
+          (<div className="m-1 rounded-full h-10 w-10 bg-[#d6480b] flex items-center justify-center text-white">
+            {user?.username?.[0] || "U"}</div>)}
         <h1 className=""> {user? user.username:"Loading... "}<h1 className="text-xs text-[#b4b4b4]">Member</h1> </h1>
         </div>
         <FiMoreVertical className=" cursor-pointer hover:bg-[#666666] rounded-full text-3xl p-1" onClick={()=> setMore(!isMore)} />
