43627dd9f8bce2e48d589958c7aab36ce493fbbe
TanmayThakur2209 <ttanmay22@gmail.com>
2025-08-03 00:31:16 +0530

    settings

---
+++ Onlyfollowers/client/src/components/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "../ui/Sidebar"
+function Settings(){
+
+    return(
+        <div className="bg-black min-h-screen ">
+        
+        <div className="z-50 fixed h-full">
+      <Sidebar/>
+      </div></div>
+    )
+}
+export default Settings
