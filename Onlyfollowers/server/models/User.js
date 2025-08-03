911286b8e939a8052e59e97af3ee539c8c7f4985
TanmayThakur2209 <ttanmay22@gmail.com>
2025-08-03 14:17:45 +0530

    ok

--- Onlyfollowers/server/models/User.js
+++ Onlyfollowers/server/models/User.js
@@ -3,19 +3,14 @@ import mongoose from "mongoose";
 const user = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  coverPhoto: {
-  filename: String,
-  path: String,
-  mimetype: String,
-  size: Number
-},
-profilePhoto: {
-  filename: String,
-  path: String,
-  mimetype: String,
-  size: Number
-}
 
+  coverPhoto: {
+    path: { type: String }, 
+  },
+  
+  profilePhoto: {
+    path: { type: String }, 
+  }
 });
 
 export default mongoose.model("User", user);
